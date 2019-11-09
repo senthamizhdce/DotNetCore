ef19e95ab24f2b27767ea9d29b92d00ec80d72db
senthamizhlcs <senthamizh.lcs@gmail.com>
2019-11-09 18:58:11 +0530

    ASPDotNet - First Commit

---
+++ ASPDotNet/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPDotNet.Startup))]
+namespace ASPDotNet
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
